4068026214208bdda1bfaaaad760245469cf831e
wlawler45 <williamgmlawler@yahoo.com>
2022-05-12 14:59:28 -0400

    Add files via upload

--- graphqlrobotserver.py
+++ graphqlrobotserver.py
@@ -29,7 +29,9 @@ def connect_failed(s, client_id, url, err):
 	error_msg="Client connect failed: " + str(client_id.NodeID) + " url: " + str(url) + " error: " + str(err)
 	print('Reconnecting')
 	fusing_obj=fusing_sub.GetDefaultClientWait(1)
-    
+
+
+#TODO implement as array with one member
 class GraphQL_sender:
     def __init__(self, args,token):
         self.current_bearer_token=token
@@ -91,18 +93,18 @@ class GraphQL_sender:
         self.perform_graphql_request(smp_query)
         smp_query = """ 
             mutation MyMutation4 {
-              updateAttribute(input: { patch: { stringValue: \""""+str(device_info.device.name)+"""\" }, id: \""""+str(self.ID_Dict["DeviceIdentifier"])+"""\" }) {
+              updateAttribute(input: { patch: { intValue: \""""+str(device_info.device.name)+"""\" }, id: \""""+str(self.ID_Dict["DeviceIdentifier"])+"""\" }) {
                 attribute {
-                 stringValue
+                 intValue
                  }
               }
             }"""
         self.perform_graphql_request(smp_query)
         smp_query = """ 
             mutation MyMutation4 {
-              updateAttribute(input: { patch: { stringValue: \""""+str(device_info.manufacturer.name)+"""\" }, id: \""""+str(self.ID_Dict["DeviceManufacturer"])+"""\" }) {
+              updateAttribute(input: { patch: { intValue: \""""+str(device_info.manufacturer.name)+"""\" }, id: \""""+str(self.ID_Dict["DeviceManufacturer"])+"""\" }) {
                 attribute {
-                 stringValue
+                 intValue
                  }
               }
             }"""
@@ -137,7 +139,7 @@ class GraphQL_sender:
         
         starting_string="["
         for i in range(len(data)):
-            starting_string+=convert_array_to_string(data[i])
+            starting_string+=self.convert_array_to_string(data[i])
             starting_string+=","
         output_string=starting_string[:-1]
         output_string+="]"
@@ -285,7 +287,7 @@ def main():
           But be aware this is short-lived (you set the expiry, see Authenticator comments below) and you will need to handle
           expiry and renewal -- as shown below. As an alternative, you could start your life-cycle with authentication, or
           you could authenticate with each request (assuming bandwidth and latency aren't factors in your use-case). '''
-    current_bearer_token = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiaW50ZXJmYWNldGVjaF9ncm91cCIsImV4cCI6MTY0Njg0MDA5NiwidXNlcl9uYW1lIjoiaW50ZXJmYWNldGVjaDI3IiwiYXV0aGVudGljYXRvciI6ImludGVyZmFjZXRlY2giLCJhdXRoZW50aWNhdGlvbl9pZCI6IjI5IiwiaWF0IjoxNjQ2ODM4Mjk2LCJhdWQiOiJwb3N0Z3JhcGhpbGUiLCJpc3MiOiJwb3N0Z3JhcGhpbGUifQ.4-bK6jJJmrCaQKf-ztOd1hCLprMg-4D_SBzSsEKBIhY"
+    current_bearer_token = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiaW50ZXJmYWNldGVjaF9ncm91cCIsImV4cCI6MTY1MjM4MTkzOSwidXNlcl9uYW1lIjoiaW50ZXJmYWNldGVjaDI3IiwiYXV0aGVudGljYXRvciI6ImludGVyZmFjZXRlY2giLCJhdXRoZW50aWNhdGlvbl9pZCI6IjMwIiwiaWF0IjoxNjUyMzgwMTM5LCJhdWQiOiJwb3N0Z3JhcGhpbGUiLCJpc3MiOiJwb3N0Z3JhcGhpbGUifQ.l8SvFnB-KoP4Y6aoQ2gZkNOLsJ1lvwkIrTFNymdSeNE"
     # eg: Bearer eyJyb2xlIjoieW91cl9yb2xlIiwiZXhwIjoxNDk5OTk5OTk5LCJ1c2VyX25hbWUiOiJ5b3VydXNlcm5hbWUiLCJhdXRoZW50aWNhdG9yIjoieW91cmF1dGgiLCJhdXRoZW50aWNhdGlvbl9pZCI6Ijk5IiwiaWF0Ijo5OTk5OTk5OTk5LCJhdWQiOiJhdWQiLCJpc3MiOiJpc3MifQ==
     
     parser = argparse.ArgumentParser()
@@ -304,14 +306,13 @@ def main():
 
     RRC.RegisterStdRobDefServiceTypes(RRN)
     robosewclient='192.168.51.61'
-    #url='rr+tcp://'+robosewclient+':58651?service=robot'://[fe80::b852:412a:6117:baa0]:11111/?
-    url='rr+tcp://[fe80::b852:412a:6117:baa0]:11111/?service=robot'
+    url='rr+tcp://'+robosewclient+':58651?service=robot'
     robot_sub=RRN.SubscribeService(url)
     robot_sub.ClientConnectFailed += connect_failed
     state_w = robot_sub.SubscribeWire("robot_state")
     cmd_w = robot_sub.SubscribeWire('position_command')
     vel_w = robot_sub.SubscribeWire("velocity_command")
-    time.sleep(1)
+    
     robot=robot_sub.GetDefaultClientWait(1)
     robot_device_info=robot.device_info
     robot_info=robot.robot_info
@@ -327,156 +328,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-    # #server_url = "opc.tcp://opcua.interfacetech.cesmii.net:51210" opc.tcp://localhost:62548/Quickstarts/DataAccessServer
-    # server_url = "opc.tcp://localhost:62548/"
-    # server_name = "ROBOT_SERVER"
-    # sample_rate = 2
-
-    # print("Configuring Simple OPC UA Server...")
-    # my_ua_server = Server()
-    # my_ua_server.set_endpoint(server_url)
-
-    # # Register a new namespace to work in
-    # my_ua_namespace = my_ua_server.register_namespace(server_name)
-    # print("Namespace ID: {}".format(my_ua_namespace))
-
-    # # Find the "Objects" folder to attach a new object to
-    # my_ua_node = my_ua_server.get_objects_node()
-    # print("Objects Node ID: ", my_ua_node)
-
-    # # Create a new object
-    # my_object = my_ua_node.add_object(my_ua_namespace, "Robot 1")
-    # print("MyObject ID:", my_object)
-
-    # # Add some attributes (of type variable) to MyObject
-    # attrib_command = my_object.add_variable(my_ua_namespace, "Command Mode", 0)
-    # attrib_operation = my_object.add_variable(my_ua_namespace, "Operational Mode", 0)
-    # attrib_con_state = my_object.add_variable(my_ua_namespace, "Controller State", 0)
-    # attrib_joint = my_object.add_variable(my_ua_namespace, "Joint Positions", 0.0)
-    # attrib_effort = my_object.add_variable(my_ua_namespace, "Joint Effort", 0.0)
-    # attrib_velocity = my_object.add_variable(my_ua_namespace, "Joint Velocity", 0.0)
-    # attrib_tcp = my_object.add_variable(my_ua_namespace, "Kin chain TCP", 0.0)
-    # attrib_tcp_vel = my_object.add_variable(my_ua_namespace, "Kin Chain TCP vel", 0.0)
-    # attrib_rob_state = my_object.add_variable(my_ua_namespace, "Robot State Flag", 0)
-    # attrib_seqno = my_object.add_variable(my_ua_namespace, "seqno", 0)
-    # attrib_joint_command = my_object.add_variable(my_ua_namespace, "Joint Position Command", 0.0)
-    # attrib_joint_velocity = my_object.add_variable(my_ua_namespace, "Joint Velocity Command", 0.0)
-    # attrib_error = my_object.add_variable(my_ua_namespace, "Current Error", "System Started but Robot Raconteur Connection Not Found")
-    # attrib_traj_run = my_object.add_variable(my_ua_namespace, "Trajectory Running", False)
-
-
-    # attrib_type = my_object.add_variable(my_ua_namespace, "Robot Type Code", 0)
-    # attrib_capabilities = my_object.add_variable(my_ua_namespace, "Robot Capabilities", 0)
-    # attrib_device = my_object.add_variable(my_ua_namespace, "Device Identifier", 0)
-    # attrib_manufacturer = my_object.add_variable(my_ua_namespace, "Device Manufacturer", "Device Manufacturer")
-
-    # # This attribute value can be written to by a client
-    # #attrib_toggler = my_object.add_variable(my_ua_namespace, "Toggle Bit", False)
-    # #attrib_toggler.set_writable()
-    # RRC.RegisterStdRobDefServiceTypes(RRN)
-    # robosewclient='192.168.51.61'
-    # url='rr+tcp://'+robosewclient+':58651?service=robot'
-    # try:
-        # my_ua_server.start()
-        # while True:
-            # attrib_command.set_value(15)
-            # time.sleep(sample_rate)
-    # except KeyboardInterrupt:
-        # print("Press Ctrl-C to terminate while statement")
-        # my_ua_server.stop()
-    # pass
-    # robot_sub=RRN.SubscribeService(url)
-    # robot_sub.ClientConnectFailed += connect_failed
-    # state_w = robot_sub.SubscribeWire("robot_state")
-    # cmd_w = robot_sub.SubscribeWire('position_command')
-    # vel_w = robot_sub.SubscribeWire("velocity_command")
-
-    # robot=robot_sub.GetDefaultClientWait(1)
-
-    # #robot_const = RRN.GetConstants("com.robotraconteur.robotics.robot", robot)
-    
-    # # state_w.WireValueChanged+=robot_state_update
-    # # cmd_w.WireValueChanged+=robot_command_update
-    # # vel_w.WireValueChanged+=robot_vel_update
-    
-
-
-    # try:
-        # print()
-        # print("Starting Simple OPC UA Server...")
-        # my_ua_server.start()
-        # print()
-        # print("Generating Sample Data...")
-        # robot_device_info=robot.device_info
-        # robot_info=robot.robot_info
-        # attrib_type.set_value(robot_info.robot_type)
-        # attrib_capabilities.set_value(robot_info.robot_capabilities)
-        # attrib_device.set_value(robot_device_info.device.name)
-        # attrib_manufacturer.set_value(robot_device_info.manufacturer.name)
-        
-        
-
-        # while True:
-            # #Update the attribute values for my object
-            # attrib_command.set_value(robot.command_mode)
-            # attrib_operation.set_value(robot.operational_mode)
-            # attrib_con_state.set_value(robot.controller_state)
-            
-            # #####attrib_error
-            
-            # robot_state=state_w.InValue
-            # attrib_seqno.set_value(robot_state.seqno)
-            # attrib_rob_state.set_value(robot_state.robot_state_flags)
-            # attrib_joint.set_value(robot_state.joint_position)
-            # attrib_effort.set_value(robot_state.joint_effort)
-            # attrib_velocity.set_value(robot_state.joint_velocity)
-            # #attrib_tcp.set_value(robot_state.kin_chain_tcp)
-            # #attrib_tcp_vel.set_value(robot_state.kin_chain_tcp_vel
-            # attrib_traj_run.set_value(robot_state.trajectory_running)
-            
-            
-            
-            # joint_command=cmd_w.InValue.command
-            # attrib_joint_command.set_value(joint_command)
-            
-            # vel_command=vel_w.InValue.command
-            # attrib_joint_velocity.set_value(vel_command)
-            # #attrib_joint.set_value(joints)
-            # #attrib_press.set_value(computer.virtual_memory.used_percent)
-
-            # print ("Current values: ", attrib_joint.get_value())
-
-            # time.sleep(sample_rate)
-    # finally:
-        # my_ua_server.stop()
-        # print("Server Offline")
-
-# def robot_state_update(w,value,time):
-    # val=w.InValue
-    # #joints=[0.0,0.0,1.0,4.0]
-    
-    # attrib_seqno.set_value(70)
-    # attrib_rob_state
-    # attrib_joint
-    # attrib_effort
-    # attrib_velocity
-    # attrib_tcp
-    # attrib_tcp_vel
-    # attrib_traj_run
-    # #attrib_joint.set_value(joints)
-    # print (val.seqno)
-    
-# def robot_command_update(w,value,time):
-    # val=w.InValue
-    # attrib_joint_command
-    # #Print the new value to the console.  Comment out this line
-    # #to see the other output more clearly
-    # print (val.seqno)
-
-# def robot_vel_update(w,value,time):
-    # val=w.InValue
-    # attrib_joint_velocity
-    # #Print the new value to the console.  Comment out this line
-    # #to see the other output more clearly
-    # print (val.seqno)
-    
